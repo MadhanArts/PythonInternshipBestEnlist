e56d4952da559f2cafbe5febac7d86847ec84c2b
MadhanArts <56270705+MadhanArts@users.noreply.github.com>
2020-11-27 12:42:43 +0530

    Add files via upload

---
+++ day5_task1.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    print("Addition of two numbers is ", a + b)
+
+
+def sub(a, b):
+    print("Subtraction of two numbers is ", a - b)
+
+
+def mul(a, b):
+    print("Multiplication of two numbers is ", a * b)
+
+
+def div(a, b):
+    print("Division of two numbers is ", a / b)
+
+
+num1 = int(input("Enter 1st number :"))
+num2 = int(input("Enter 2nd number :"))
+add(num1, num2)
+sub(num1, num2)
+mul(num1, num2)
+div(num1, num2)
---
+++ day5_task2.py
@@ -0,0 +1,7 @@
+def covid(patient_name, temperature=98):
+    print("Patient name : ", patient_name)
+    print("Body Temperature : ", temperature)
+
+
+covid("Madhan")
+covid("Ritesh", 95)
