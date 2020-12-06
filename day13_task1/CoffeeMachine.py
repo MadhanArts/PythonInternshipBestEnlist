0697b180ce5be5c6fa29e4d09594d0639a878d94
MadhanArts <56270705+MadhanArts@users.noreply.github.com>
2020-12-06 19:16:34 +0530

    Update CoffeeMachine.py

--- day13_task1/CoffeeMachine.py
+++ day13_task1/CoffeeMachine.py
@@ -15,19 +15,20 @@ class CoffeeMachine:
 
     def is_resource_sufficient(self, drink):
         is_sufficient = True
-        if self.coffee < drink["coffee"]:
+        if self.coffee < drink.coffee:
             is_sufficient = False
             print("Sorry there is not enough coffee")
-        if self.milk < drink["milk"]:
+        if self.milk < drink.milk:
             is_sufficient = False
             print("Sorry there is not enough milk")
-        if self.water < drink["water"]:
+        if self.water < drink.water:
             is_sufficient = False
             print("Sorry there is not enough water")
 
         return is_sufficient
 
     def process_coins(self, drink):
+        print("It cost you $", drink.money, " sir", sep="")
         print("Insert Coins : ")
         try:
             quarters = int(input("Enter number of quarters : "))
@@ -39,20 +40,20 @@ class CoffeeMachine:
             return False
 
         total = 0.25 * quarters + 0.1 * dimes + 0.05 * nickles + 0.01 * pennies
-        if total < drink["money"]:
+        if total < drink.money:
             print("Sorry that's not enough money. Money refunded")
             return False
-        elif total == drink["money"]:
-            self.money += drink["money"]
+        elif total == drink.money:
+            self.money += drink.money
             return True
         else:
-            self.money += drink["money"]
-            print("Here is ", "{:.2f}".format(total - drink["money"]), " dollars in change", sep="")
+            self.money += drink.money
+            print("Here is ", "{:.2f}".format(total - drink.money), " dollars in change", sep="")
             return True
 
     def make(self, drink):
         if self.is_resource_sufficient(drink) and self.process_coins(drink):
-            self.water -= drink["water"]
-            self.milk -= drink["milk"]
-            self.coffee -= drink["coffee"]
-            print("Here is your ", drink["name"], ". Enjoy!", sep="")
+            self.water -= drink.water
+            self.milk -= drink.milk
+            self.coffee -= drink.coffee
+            print("Here is your ", drink.name, ". Enjoy!", sep="")
