e8544f43278a8477f8380c008346d2cadf61b9bb
MadhanArts <56270705+MadhanArts@users.noreply.github.com>
2020-12-06 19:16:03 +0530

    Update main.py

--- day13_task1/main.py
+++ day13_task1/main.py
@@ -1,22 +1,25 @@
 from day13_task1.CoffeeMachine import CoffeeMachine
-from day13_task1.drinks import drinks
+from day13_task1.Drink import Drink
 
 my_machine = CoffeeMachine(3000, 2000, 1000, 0)
+latte = Drink("Latte", 200, 150, 24, 2.5)
+espresso = Drink("Espresso", 150, 100, 30, 3)
+cappuccino = Drink("Cappuccino", 150, 175, 20, 4)
 
 while True:
     print("****** Welcome sir ******")
     print("What would you like? ([1] espresso/[2] latte/[3] cappuccino)")
-    choice = input("Enter your choice or option : ")
+    choice = input("Enter your choice or option : ").lower().strip()
 
-    if choice.lower() == "espresso" or "1":
-        my_machine.make(drinks["espresso"])
-    elif choice.lower() == "latte" or "2":
-        my_machine.make(drinks["latte"])
-    elif choice.lower() == "cappuccino" or "3":
-        my_machine.make(drinks["cappuccino"])
-    elif choice.lower() == "report":
+    if choice in ("espresso", "1"):
+        my_machine.make(espresso)
+    elif choice in ("latte", "2"):
+        my_machine.make(latte)
+    elif choice in ("cappuccino", "3"):
+        my_machine.make(cappuccino)
+    elif choice == "report":
         my_machine.generate_report()
-    elif choice.lower() == "off":
+    elif choice == "off":
         exit(1)
     else:
         print("No option like that sir! Try again")
