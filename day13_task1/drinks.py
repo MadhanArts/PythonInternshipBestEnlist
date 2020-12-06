a719ce7b46a8e72fcbaa87ce816e4f75dbf8f55b
MadhanArts <56270705+MadhanArts@users.noreply.github.com>
2020-12-06 19:17:45 +0530

    Update and rename drinks.py to Drink.py

--- day13_task1/drinks.py
+++
@@ -1,4 +0,0 @@
-latte = {"name": "Latte", "water": 200, "milk": 150, "coffee": 24, "money": 2.5}
-espresso = {"name": "Espresso", "water": 150, "milk": 100, "coffee": 30, "money": 3}
-cappuccino = {"name": "Cappuccino", "water": 150, "milk": 175, "coffee": 20, "money": 4}
-drinks = {"latte": latte, "espresso": espresso, "cappuccino": cappuccino}
