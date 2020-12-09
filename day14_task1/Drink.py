78d047c41fd92e36683315755cd3bd1921955cba
MadhanArts <56270705+MadhanArts@users.noreply.github.com>
2020-12-09 23:54:14 +0530

    Create Drink.py

---
+++ day14_task1/Drink.py
@@ -0,0 +1,8 @@
+
+class Drink:
+    def __init__(self, name, water, milk, coffee, money):
+        self.name = name
+        self.water = water
+        self.milk = milk
+        self.coffee = coffee
+        self.money = money
